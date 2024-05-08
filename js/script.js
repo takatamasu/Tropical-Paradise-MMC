1716adfa4ecdda9b24a24e124241ddc69e238eb9
Asu <asunohanna@gmail.com>
2024-05-08 13:55:26 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+	handleNavItemsAnimation()
+	deleteAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		++delayTime
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
+
+const deleteAnimation = () => {
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			allNavItems.forEach(el => {
+				el.classList.remove('nav-items-animation')
+			})
+		})
+	})
+}
