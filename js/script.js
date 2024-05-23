f30f4205fb5787bc9e0118fa58d698081256176c
Asu <asunohanna@gmail.com>
2024-05-23 14:14:25 +0200

    Change BtnBars color on scroll in .js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+
+	navBtnBars.classList.remove('black-bars-color')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
@@ -35,3 +40,26 @@ const deleteAnimation = () => {
 		})
 	})
 }
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (
+			section.classList.contains('white-section') &&
+			(section.offsetTop <= currentSection + 60) & !nav.classList.contains('nav--active')
+		) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
+window.addEventListener('scroll', handleObserver)
